a7a3ca980d06c8195a73ce2338e45cfa33aeaae0
Kirill Dolzhenkov <k.dolzhenkov@gmail.com>
2021-11-25 23:33:18 +0300

    modified edit mode Fn

--- src/EdittableSpan.tsx
+++ src/EdittableSpan.tsx
@@ -9,27 +9,41 @@ type EdittableSpanType = {
 
 const EdittableSpan: React.FC<EdittableSpanType> = (props)=> {
     const[editMode, setEditMode] = useState(false);
-    const[title, setTitle] = useState("")
+    const[title, setTitle] = useState("");
 
-    const onEditMode = () => {
-        setEditMode(true)
-        setTitle(props.title)
-    };
+    /*const onEditMode = () => {
+        setEditMode(true);
+        setTitle(props.title);
+    }
     const offEditMode = () => {
         setEditMode(false);
         props.onChangeTitle(title);
+    }*/
+
+    const changeEditMode = (value: boolean) => {
+        setEditMode(value);
+        if (value) {
+            setTitle(props.title);
+        } else {
+            props.onChangeTitle(title);
+        }
     }
 
     const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-        setTitle(e.currentTarget.value)
+        setTitle(e.currentTarget.value);
     }
 
     return(
         <>
             {
                 editMode
-                ? <input value={title} onChange={onChangeHandler} onBlur={offEditMode} autoFocus={true}></input>
-                : <span onDoubleClick={onEditMode} >{props.title}</span>
+                ? <input
+                        value={title}
+                        onChange={onChangeHandler}
+                        onBlur={()=>changeEditMode(false)}
+                        autoFocus={true}>
+                    </input>
+                : <span onDoubleClick={()=>changeEditMode(true)} >{props.title}</span>
             }
         </>
     )
