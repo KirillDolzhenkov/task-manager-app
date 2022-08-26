b228537be7a74dca521d4462ea91bf4fe209579c
Kirill Dolzhenkov <k.dolzhenkov@gmail.com>
2022-08-26 18:27:44 +0300

    Updates

---
+++ precache-manifest.db9226608f1052f20b2ed5d9917bf1e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa38a277ebe1f7f6fafc9847632a84a2",
+    "url": "/test-todolist/index.html"
+  },
+  {
+    "revision": "18e75e9bbe9ae4774781",
+    "url": "/test-todolist/static/css/main.de4bbd02.chunk.css"
+  },
+  {
+    "revision": "cd7465dd04e19fed7f69",
+    "url": "/test-todolist/static/js/2.a1b6a7de.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/test-todolist/static/js/2.a1b6a7de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18e75e9bbe9ae4774781",
+    "url": "/test-todolist/static/js/main.dda99b93.chunk.js"
+  },
+  {
+    "revision": "767660a26794efc8f6d3",
+    "url": "/test-todolist/static/js/runtime-main.d1bd818c.js"
+  }
+]);
