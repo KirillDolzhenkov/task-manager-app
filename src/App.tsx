d062e3259fcb933d1a66462ee02e067ba2bb1ccd
Kirill Dolzhenkov <k.dolzhenkov@gmail.com>
2024-08-16 15:53:58 +0300

    re-init project

--- src/App.tsx
+++ src/App.tsx
@@ -1,112 +1,35 @@
-import React, {useCallback} from 'react';
-import Container from "@mui/material/Container";
-import Grid from '@mui/material/Grid'
-import Paper from '@mui/material/Paper';
-import {useDispatch, useSelector} from "react-redux";
-
-import './App.css';
-import {Todolist} from './Todolist';
-import {AddItemForm} from "./components/AddItemForm";
-import {ButtonAppBar} from "./ButtonAppBar";
-import {
-    addTodoListAC,
-    changFilterValueAC,
-    removeTodoListAC,
-    updateTodoListTitleAC
-} from "./redusers/TodolistReducer";
-import {
-    addTaskAC,
-    changeTaskStatusAC,
-    removeTaskAC,
-    updateTaskTitleAC
-} from "./redusers/TasksReducer";
-import {RootReducerType} from "./store/store";
-
-export type TasksType = {
-    taskId: string
-    isDone: boolean
-    name: string
-}
-
-export type TodoListsType = {
-    todoId: string
-    name: string
-    filterValue: FilterValueType
-}
-
-export type TasksStateType = {
-    [todoId: string] : TasksType[]
-}
-
-export type FilterValueType = "all" | "active" | "completed"
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
 
-    console.log("App")
-
-    const dispatch = useDispatch();
-    const todoLists = useSelector<RootReducerType, TodoListsType[]>((state) => state.todoLists);
-    const tasks = useSelector<RootReducerType, TasksStateType>((state) => state.tasks);
-
-    const addTodoList = useCallback((title: string) => {
-        const action = addTodoListAC(title);
-        dispatch(action);
-    },[dispatch]);
-
-    const removeTodoLIst = useCallback((todoId: string) => {
-        const action = removeTodoListAC(todoId);
-        dispatch(action);
-    },[dispatch]);
-
-    const changFilterValue = useCallback((todoId: string, filterValue: FilterValueType) => {
-        dispatch(changFilterValueAC(todoId, filterValue));
-    },[dispatch]);
-
-    const updateTodoListTitle = useCallback((todoId: string, title: string) => {
-        dispatch(updateTodoListTitleAC(todoId, title))
-    },[dispatch]);
-
-
-
-
-
-
-    const mappedTodoLists: JSX.Element[] = todoLists.map(tl => {
-        let tasksForTodoList: TasksType[] = tasks[tl.todoId];
-        return (
-            <Grid item key={tl.todoId}>
-                <Paper elevation={5} sx={{padding: "20px"}}>
-                    <Todolist
-                        /*key={tl.TodoId}*/
-                        TodoId={tl.todoId}
-                        tasks={tasksForTodoList}
-                        name={tl.name}
-                        filter={tl.filterValue}
-                        changFilterValue={changFilterValue}
-                        removeTodoLIst={removeTodoLIst}
-                        changeTodoTitle={updateTodoListTitle}
-                    />
-                </Paper>
-            </Grid>
-        )
-    });
-
-
-    return (
-        <div className="App">
-            <ButtonAppBar/>
-            <Container fixed>
-                <Grid container sx={{padding: "20px"}}>
-                    <Grid item>
-                        <AddItemForm addItem={addTodoList}/>
-                    </Grid>
-                </Grid>
-                <Grid container spacing={3}>
-                    {mappedTodoLists}
-                </Grid>
-            </Container>
-        </div>
-    );
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
